63128f9562f8061c1b64698dd915920aa5f3e8f4
Serje <Serje.havandjian@gmail.com>
2022-05-18 14:10:02 -0400

    commit tree d03bb94e4894f6f898adbf4411393ef26bc768e4

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [cartItem, ItemAdder] = useState("not-cart")
+  const changeCartItem = cartItem ? "in-cart" : "not-cart"
+
+
+  const [buttonStatus, ButtonChanger] = useState("add")
+  const changeButton = buttonStatus ? "remove" : "add"
+
+
+  function addItemFunction(){
+    ItemAdder(changeCartItem)
+    ButtonChanger(changeButton)
+    }
+
   return (
-    <li className="">
+    <li className={cartItem}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonStatus} onClick={addItemFunction}>Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,10 +2,33 @@ import React from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const foods = items.map((item)=>{
+    return item.name
+  })
+
+  console.log(foods)
+
+  const [selectedCategory, changeCategory] = useState("All")
+
+  changeCategory(event.target.value)
+
+  function handleSelect(){
+    changeCategory( foodsToDisplay = foods.filter((food)=>{
+        if (selectedCategory === "All"){
+          return true
+        } else{
+          return food.category
+        }
+      })
+    )
+  }
+
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onClick={handleSelect}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -16,6 +39,7 @@ function ShoppingList({ items }) {
         {items.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
+        <li>{foodsToDisplay}</li>
       </ul>
     </div>
   );
